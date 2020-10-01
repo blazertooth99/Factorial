2ee8ef2637a905d6d0c115618831a8a3c9c9007a
Viraj Sapte <63946031+virajsapte@users.noreply.github.com>
2020-10-01 09:10:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = int(input())
-fact = 1
+n = int(input("Enter a number: "))
+factorial = 1
 
 if(n == 0 or n==1):
     print(1)
@@ -7,4 +7,4 @@ else:
     for i in range(1,n+1):
         fact = fact * i
         
-print(fact)
+print(factorial)
